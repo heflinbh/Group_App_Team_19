94315bfe5ee0863d1adc383e2b81b46f152a3686
heflinbh <76850289+heflinbh@users.noreply.github.com>
2022-03-31 20:52:05 +0100

    Connected Login to Main Activity

--- TheWorstGame/app/src/main/java/edu/neu/theworstgame/MainActivity.java
+++ TheWorstGame/app/src/main/java/edu/neu/theworstgame/MainActivity.java
@@ -2,13 +2,39 @@ package edu.neu.theworstgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    String callSign;
+    String unitDesignation;
+
+    TextView welcomeTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Bundle intent = getIntent().getExtras();
+        callSign = intent.getString("Call Sign");
+        unitDesignation = intent.getString("Unit Designation");
+
+        welcomeTextView = findViewById(R.id.welcomeTextView);
+        welcomeTextView.setText("Back again, " + callSign + "?");
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.startNewMissionButton:
+
+                Intent assessmentActivity = new Intent(getApplicationContext(), AssessmentActivity.class);
+                startActivity(assessmentActivity);
+
+                break;
+        }
     }
 }
