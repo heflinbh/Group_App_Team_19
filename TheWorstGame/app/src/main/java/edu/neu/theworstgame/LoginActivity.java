aab56ba9f4f2d04c25f5eb6dd4f6bc0334b64c77
chenyangw <chenyang.wang1021@gmail.com>
2022-04-24 02:48:15 -0700

    login activities; update user class

--- TheWorstGame/app/src/main/java/edu/neu/theworstgame/LoginActivity.java
+++ TheWorstGame/app/src/main/java/edu/neu/theworstgame/LoginActivity.java
@@ -1,15 +1,24 @@
 package edu.neu.theworstgame;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
+    private static final String TAG = "LoginActivity";
+    public User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,21 +32,94 @@ public class LoginActivity extends AppCompatActivity {
 
         textInput = (TextInputLayout) findViewById(R.id.unitDesignationTextInput);
         String unitDesignation = textInput.getEditText().getText().toString();
+        DatabaseReference myRef = FirebaseDatabase.getInstance().getReference("users");
 
         switch (view.getId()) {
             case R.id.loginButton:
                 Intent mainScreenActivity = new Intent(getApplicationContext(), HomeActivity.class);
                 mainScreenActivity.putExtra("Call Sign", callSign);
                 mainScreenActivity.putExtra("Unit Designation", unitDesignation);
-                startActivity(mainScreenActivity);
+
+                myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (snapshot.hasChild(callSign)) {
+                            myRef.child(callSign).addValueEventListener(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                    user = snapshot.getValue(User.class);
+                                    if (!unitDesignation.equals(user.getPassword())){
+                                        Toast.makeText(LoginActivity.this, "Password incorrect.", Toast.LENGTH_SHORT).show();
+                                    } else {
+                                        Toast.makeText(LoginActivity.this, "Logged in.", Toast.LENGTH_SHORT).show();
+                                        startActivity(mainScreenActivity);
+                                    };
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError error) {
+
+                                }
+                            });
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Username does not exist.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
                 break;
 
             case R.id.signUpButton:
-                Intent storyActivity = new Intent(getApplicationContext(), StoryActivity.class);
-                storyActivity.putExtra("Call Sign", callSign);
-                storyActivity.putExtra("Unit Designation", unitDesignation);
-                startActivity(storyActivity);
+                Intent storyStart = new Intent(getApplicationContext(), StoryStart.class);
+                storyStart.putExtra("Call Sign", callSign);
+                storyStart.putExtra("Unit Designation", unitDesignation);
+
+                myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (snapshot.hasChild(callSign)){
+                            Toast.makeText( LoginActivity.this, "User existed. Please login.", Toast.LENGTH_SHORT).show();
+                        }else {
+                            createNewUser(callSign, callSign, unitDesignation);
+                            startActivity(storyStart);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
                 break;
         }
     }
+
+
+    /**
+     * create initial user and add to firebase
+     * @param username
+     * @param display_name
+     * @param password
+     */
+    public void createNewUser(String username, String display_name, String password) {
+        DatabaseReference myRef = FirebaseDatabase.getInstance().getReference("users");
+        User user = new User(username
+                , password
+                ,null
+                , display_name
+                , -1
+                , 0
+                , 0
+        );
+        myRef.child(username).setValue(user);
+
+        Toast.makeText( this, "User created.", Toast.LENGTH_SHORT).show();
+    }
+
 }
--- TheWorstGame/app/src/main/java/edu/neu/theworstgame/User.java
+++ TheWorstGame/app/src/main/java/edu/neu/theworstgame/User.java
@@ -7,12 +7,49 @@ public class User {
     private String username;
     private ArrayList<Mission> completedMissions;
     // Haven't decided how to implement tier yet
+    String password;
     private String tier;
+    private String display_name;
+    private long last_mission_timestamp;
+    private int mission_accomplished;
+    private int points;
 
+    public User() {
+    };
+
+    public User(String username
+            , String password
+            , String tier
+            , String display_name
+            , long last_mission_timestamp
+            , int mission_accomplished
+            , int points
+    ) {
+        this.username = username;
+        this.password = password;
+        this.tier = tier;
+        this.display_name = display_name;
+        this.completedMissions = new ArrayList<>();
+        this.last_mission_timestamp = last_mission_timestamp;
+        this.mission_accomplished = mission_accomplished;
+        this.points = points;
+    }
+
+    /**
+     * Initial user with no mission history
+     * no display name entered use username
+     * @param username
+     * @param tier
+     */
     public User(String username, String tier) {
         this.username = username;
+        this.password = "";
         this.tier = tier;
+        this.display_name = username;
         this.completedMissions = new ArrayList<>();
+        this.last_mission_timestamp = -1;
+        this.mission_accomplished = 0;
+        this.points = 0;
     }
 
     public void addCompletedMission(Mission mission) {
@@ -27,5 +64,24 @@ public class User {
         return username;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public String getDisplayName() {
+        return display_name;
+    }
+
+    public long getLastMissionTimestamp() {
+        return last_mission_timestamp;
+    }
+
+    public int getMissionAccomplished() {
+        return mission_accomplished;
+    }
+
+    public int getPoints() {
+        return points;
+    }
 
 }
