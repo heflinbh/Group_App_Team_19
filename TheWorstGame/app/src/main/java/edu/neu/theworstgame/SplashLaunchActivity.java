b558722443cd77569e094067cdbf55008b554e06
Yuan Cao <yuancao85@gmail.com>
2022-05-01 14:45:59 -0400

    Update splash screen

--- TheWorstGame/app/src/main/java/edu/neu/theworstgame/SplashLaunchActivity.java
+++ TheWorstGame/app/src/main/java/edu/neu/theworstgame/SplashLaunchActivity.java
@@ -1,10 +1,13 @@
 package edu.neu.theworstgame;
 
+import static edu.neu.theworstgame.R.drawable.*;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ImageView;
 
 import java.io.File;
 
@@ -18,12 +21,55 @@ public class SplashLaunchActivity extends AppCompatActivity {
 
         getSupportActionBar().hide();
 
+        int base = 300;
+
         // Determine if this is the first time playing the game
         File file = getApplicationContext().getFileStreamPath(FILE_NAME);
         boolean first_time = !file.exists();
 
-        Handler h = new Handler();
-        h.postDelayed(new Runnable() {
+        ImageView iv = findViewById(R.id.imageView);
+        Handler h1 = new Handler();
+        h1.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                iv.setImageResource(neptune);
+            }
+        }, 1 * base);
+
+        Handler h2 = new Handler();
+        h2.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                iv.setImageResource(neptune);
+            }
+        }, 2 * base);
+
+        Handler h3 = new Handler();
+        h3.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                iv.setImageResource(mercury);
+            }
+        }, 3 * base);
+
+        Handler h4 = new Handler();
+        h4.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                iv.setImageResource(aries);
+            }
+        }, 4 * base);
+
+        Handler h5 = new Handler();
+        h5.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                iv.setImageResource(earth);
+            }
+        }, 5 * base);
+
+        Handler h6 = new Handler();
+        h6.postDelayed(new Runnable() {
             @Override
             public void run() {
                 Intent i;
@@ -35,7 +81,7 @@ public class SplashLaunchActivity extends AppCompatActivity {
                 startActivity(i);
                 finish();
             }
-        }, 2000);
+        }, 6 * base);
 
         // Start background service in an separate thread
     }
